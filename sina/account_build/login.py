545d5eea7787edba2ee27e80959e3a55e0a78ec8
hzh <huang_zhihao@163.com>
2020-08-13 10:42:53 +0000

    use chrome to replace PhantomJS which is no longer supported

--- sina/account_build/login.py
+++ sina/account_build/login.py
@@ -7,20 +7,58 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import sys
 import time
+import random
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from scrapy.http.response.html import HtmlResponse
+from selenium.webdriver.chrome.options import Options
 
-sys.path.append(os.getcwd())
+
+# print(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+# sys.path.append(os.getcwd())
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../..")))
 from sina.settings import LOCAL_MONGO_HOST, LOCAL_MONGO_PORT, DB_NAME
 
 TEMPLATES_FOLDER = os.getcwd() + '/sina/account_build/templates/'
 
+#  随机请求头设置
+USER_AGENTS = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; Avant Browser; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)',
+        'Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0',
+        'Mozilla/5.0 (X11; Linux i586; rv:63.0) Gecko/20100101 Firefox/63.0',
+        'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.10; rv:62.0) Gecko/20100101 Firefox/62.0'
+    ]
 
 class WeiboLogin():
     def __init__(self, username, password):
-        os.system('pkill -f phantom')
+        # os.system('pkill -f phantom')
         self.url = 'https://passport.weibo.cn/signin/login?entry=mweibo&r=https://weibo.cn/'
         # 这里需要修改，给出自己电脑phantomjs.exe的位置
-        self.browser = webdriver.PhantomJS(executable_path=r'D:\phantomjs-2.1.1-windows\bin\phantomjs.exe')
-        self.browser.set_window_size(1050, 840)
+        headers = {}
+        headers['User-Agent'] = random.choice(USER_AGENTS)
+        chrome_options = Options()
+        # headers = random.choice(USER_AGENTS)
+        chrome_options.add_argument('--user-agent={}'.format(headers))  # 设置请求头的User-Agent
+        chrome_options.add_argument('--blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
+        chrome_options.add_argument('--headless')  # 浏览器不提供可视化页面
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+
+        self.browser = webdriver.Chrome(chrome_options=chrome_options)
+        self.browser.maximize_window()
+        # self.browser = webdriver.PhantomJS()
+        # self.browser.set_window_size(1050, 840)
         self.wait = WebDriverWait(self.browser, 20)
         self.username = username
         self.password = password
@@ -55,11 +93,16 @@ class WeiboLogin():
 
 
 def login_weibo():
-
     print("当前时间为：{}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-    file_path = './account.txt'
+    # file_path = './account.txt'
+    # print(os.getcwd())
+    # print("*" * 30)
+    # print(os.listdir(os.getcwd()))
+    # print(open("account.txt").read())
+    file_path = 'account.txt'
     with open(file_path, 'r') as f:
         lines = f.readlines()
+        # print(lines)
     mongo_client = pymongo.MongoClient(LOCAL_MONGO_HOST, LOCAL_MONGO_PORT)
     collection = mongo_client[DB_NAME]["account"]
     count = 0
@@ -85,11 +128,10 @@ def login_weibo():
         print('获取第{}个账号的cookie成功，耗时：{}'.format(count, end - start))
         print('Cookie:', cookie_str)
         try:
-            collection.insert(
+            collection.insert_one(
                 {"_id": username, "password": password, "cookie": cookie_str, "status": "success"})
         except DuplicateKeyError as e:
             collection.find_one_and_update({'_id': username}, {'$set': {'cookie': cookie_str, "status": "success"}})
-
 
 
 def judge():
@@ -99,7 +141,7 @@ def judge():
         print("现在开始判断：")
         mongo_client = pymongo.MongoClient(LOCAL_MONGO_HOST, LOCAL_MONGO_PORT)
         collection = mongo_client[DB_NAME]["account"]
-        all_count = collection.find({'status': 'success'}).count()
+        all_count = collection.count_documents({'status': 'success'})
         if all_count < 70:
             print("需重新获取cookie！！！重新获取中")
             print("=" * 30)
